417e39ea4b10a9b2b681c89769c237e403aa3f49
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-02-15 22:16:10 -0500

    more database stuff

---
+++ import.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+
+import os
+import sys
+import optparse
+from lxml import etree
+
+import model
+
+def parse_args():
+    p = optparse.OptionParser()
+    p.add_option('--dburi', default='sqlite:///envoy.db')
+    p.add_option('--debug', action='store_true')
+    return p.parse_args()
+
+def main():
+    opts, args = parse_args()
+
+    session = model.init(opts.dburi, echo=opts.debug)
+
+    for datafile in args:
+        data = etree.parse(open(datafile))
+
+        node_report = data.getroot()
+        report = session.query(model.Report).get(
+                node_report.get('report_timestamp'))
+
+        if report:
+            continue
+
+        report = model.Report(report_timestamp =
+                node_report.get('report_timestamp'))
+
+        node_envoy = node_report.find('envoy')
+
+        envoy = session.query(model.Envoy).get(node_envoy.get('serial_num'))
+        if not envoy:
+            envoy = model.Envoy(ip_addr = node_envoy.get('ip_addr'),
+                    mac_addr = node_envoy.get('mac_addr'),
+                    timezone = node_envoy.get('timezone'),
+                    serial_num = node_envoy.get('serial_num'),
+                    sw_version = node_envoy.get('sw_version'))
+
+            session.add(envoy)
+
+        for node_device in node_report.findall('device'):
+            device = session.query(model.Device).get(node_device.get('eqid'))
+            if not device:
+                device = model.Device(eqid = node_device.get('eqid'),
+                        part_num=node_device.get('part_num'),
+                        device_type=node_device.get('device_type'),
+                        envoy=envoy)
+                session.add(device)
+
+            device.image_bits = node_device.get('image_bits')
+            device.admin_state = node_device.get('admin_state')
+            device.condition_flags = node_device.get('condition_flags')
+            device.observed_flags = node_device.get('observed_flags')
+            device.control_bits = node_device.get('control_bits')
+
+        for node_interval in node_report.findall('interval'):
+            interval = session.query(model.Interval).get(
+                    node_interval.get('id'))
+            if interval:
+                continue
+
+            interval = model.Interval(
+                    report=report,
+                    id=node_interval.get('id'),
+                    stat_duration=node_interval.get('stat_duration'),
+                    interval_duration=node_interval.get('interval_duration'),
+                    eqid=node_interval.get('eqid'),
+                    end_date=node_interval.get('end_date'))
+
+            stats = model.IntervalStats(
+                    interval=interval)
+
+            for i,v in enumerate(node_interval.get('stats').split(',')):
+                setattr(stats, 'val%d' % i, v)
+
+            session.add(stats)
+            session.add(interval)
+
+        for node_event in node_report.findall('event'):
+            event = session.query(model.Event).get(
+                    node_event.get('id'))
+            if event:
+                continue
+
+            event = model.Event(
+                    id = node_event.get('id'),
+                    correlation_id = node_event.get('correlation_id'),
+                    event_state = node_event.get('event_state'),
+                    event_code = node_event.get('event_code'),
+                    eqid = node_event.get('eqid'),
+                    serial_num = node_event.get('serial_num'),
+                    event_date = node_event.get('event_date'),
+                    )
+
+            device = session.query(model.Device).get(event.serial_num)
+            if device:
+                device.events.append(event)
+
+            session.add(event)
+
+        envoy.reports.append(report)
+        session.add(report)
+        session.commit()
+
+if __name__ == '__main__':
+    main()
+
+
--- model.py
+++ model.py
@@ -1,57 +1,79 @@
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 
 Base = declarative_base()
 
 class TableNameMixin (object):
 
-	@declared_attr
-	def __tablename__(cls):
-		return cls.__name__.lower()
-
-
-class Report (TableNameMixin,Base):
-	report_timestamp = Column(Integer, primary_key=True)
-	pass
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
 
 class Envoy (TableNameMixin,Base):
-	serial_num = Column(String, primary_key=True)
-	ip_addr = Column(String)
-	mac_addr = Column(String)
-	part_num = Column(String)
-	timezone = Column(String)
-	pass
+    serial_num = Column(String, primary_key=True)
+    ip_addr = Column(String)
+    mac_addr = Column(String)
+    part_num = Column(String)
+    timezone = Column(String)
+    sw_version = Column(String)
+    reports = relationship('Report', backref='envoy')
+    devices = relationship('Device', backref='envoy')
+
+class Report (TableNameMixin,Base):
+    report_timestamp = Column(Integer, primary_key=True)
+    envoy_id = Column(String, ForeignKey('envoy.serial_num'))
+    intervals = relationship('Interval', backref='report')
 
 class Device (TableNameMixin,Base):
-	eqid = Column(String, primary_key=True)
-	image_bits = Column(Integer)
-	control_bits = Column(Integer)
-	device_type = Column(Integer)
-	admin_state = Column(Integer)
-	condition_flags = Column(Integer)
-	part_num = Column(String)
-	pass
+    eqid = Column(String, primary_key=True)
+    image_bits = Column(Integer)
+    control_bits = Column(Integer)
+    device_type = Column(Integer)
+    admin_state = Column(Integer)
+    condition_flags = Column(Integer)
+    part_num = Column(String)
+    events = relationship('Event', backref='device')
+    envoy_id = Column(String, ForeignKey('envoy.serial_num'))
 
 class Interval (TableNameMixin,Base):
-	id = Column(Integer, primary_key=True)
-	stat_duration = Column(Integer)
-	interval_duration = Column(Integer)
-	eqid = Column(String)
-	pass
+    id = Column(Integer, primary_key=True)
+    stat_duration = Column(Integer)
+    interval_duration = Column(Integer)
+    eqid = Column(String)
+    end_date = Column(Integer)
+    stats = relationship('IntervalStats', backref='interval')
+    report_id = Column(Integer, ForeignKey('report.report_timestamp'))
 
 class IntervalStats (TableNameMixin, Base):
-	id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
-	val0 = Column(Integer)
-	val1 = Column(Integer)
-	val2 = Column(Integer)
-	val3 = Column(Integer)
-	val4 = Column(Integer)
-	val5 = Column(Integer)
-	val6 = Column(Integer)
-	val7 = Column(Integer)
+    val0 = Column(Integer)
+    val1 = Column(Integer)
+    val2 = Column(Integer)
+    val3 = Column(Integer)
+    val4 = Column(Integer)
+    val5 = Column(Integer)
+    val6 = Column(Integer)
+    val7 = Column(Integer)
+
+    interval_id = Column(Integer, ForeignKey('interval.id'))
 
 class Event (TableNameMixin,Base):
-	id = Column(Integer, primary_key=True)
-	pass
+    id = Column(Integer, primary_key=True)
+
+    correlation_id = Column(Integer)
+    event_state = Column(Integer)
+    event_code = Column(Integer)
+    eqid = Column(Integer)
+    serial_num = Column(Integer)
+    event_date = Column(Integer)
+
+    device_id = Column(Integer, ForeignKey('device.eqid'))
+
+def init(dburi = 'sqlite:///:memory:', echo=False):
+    engine = create_engine(dburi, echo=echo)
+    Base.metadata.create_all(engine)
+    factory = sessionmaker(bind=engine)
+    return factory()
 
